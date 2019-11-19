93008e87fe94321101e7d0924e70eba7593b5cde
Houssnou <hosni.moubarak@gmail.com>
2019-11-19 10:57:00 -0500

    Project Skeleton

---
+++ book-service/src/main/java/com/company/bookservice/controller/bookServiceController.java
@@ -0,0 +1,4 @@
+package com.company.bookservice.controller;
+
+public class bookServiceController {
+}
---
+++ note-service/src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+create schema if not exists note;
+use note;
+create table if not exists note (
+	note_id int not null auto_increment primary key,
+    book_id int not null,
+    note varchar(255)
+);
